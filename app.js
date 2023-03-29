217e14a2e09d599293835289cbb42dd1b3dfdf8c
Druhin Tarafder <druhinpro@gmail.com>
2023-03-29 15:40:09 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,37 +1,82 @@
 function convert() {
-    const targetSize =
-        parseInt(document.getElementById("targetSizeInput").value) * 1024;
-    const nameConvention = document.getElementById("nameInput").value;
-    const files = document.getElementById("fileInput").files;
+    const targetSize = parseInt(document.getElementById('targetSizeInput').value) * 1024; // convert to bytes
+    const namingConvention = document.getElementById('nameInput').value;
+    const files = document.getElementById('fileInput').files;
+    const outputDir = "output"; // name of output directory
+
+    // create output directory if it doesn't exist
+    if (!window.File) {
+        window.alert('Your browser does not support the File API!');
+        return;
+    }
+
+    // create output directory if it doesn't exist
+    if (!window.File.prototype.slice) {
+        Object.defineProperty(window.File.prototype, 'slice', {
+            value: window.File.prototype.webkitSlice || window.File.prototype.mozSlice
+        });
+    }
+
+    if (!window.FileReader) {
+        window.alert('Your browser does not support the FileReader API!');
+        return;
+    }
+
+    if (!window.Blob) {
+        window.alert('Your browser does not support the Blob API!');
+        return;
+    }
+
+    if (!document.createElement('canvas').getContext) {
+        window.alert('Your browser does not support canvas!');
+        return;
+    }
+
+    if (!document.createElement('canvas').toBlob) {
+        window.alert('Your browser does not support toBlob()!');
+        return;
+    }
+
+    if (!document.createElement('a').download) {
+        window.alert('Your browser does not support the download attribute!');
+        return;
+    }
+
+    if (!document.createElement('a').click) {
+        window.alert('Your browser does not support the click() method on anchors!');
+        return;
+    }
+
+    if (!fs.existsSync(outputDir)) {
+        fs.mkdirSync(outputDir);
+    }
 
     for (let i = 0; i < files.length; i++) {
         const file = files[i];
         const img = new Image();
 
         img.onload = function () {
-            const canvas = document.createElement("canvas");
-            const scaleFactor = targetSize / (file.size || file.blob.size);
-            const width = img.width * scaleFactor;
-            const height = img.height * scaleFactor;
+            const canvas = document.createElement('canvas');
+            const maxSize = Math.sqrt(targetSize * Math.pow(img.width / img.height, 2));
+            const width = img.width > img.height ? maxSize : targetSize / maxSize;
+            const height = img.width > img.height ? targetSize / maxSize : maxSize;
+
             canvas.width = width;
             canvas.height = height;
-            const ctx = canvas.getContext("2d");
+
+            const ctx = canvas.getContext('2d');
             ctx.drawImage(img, 0, 0, width, height);
 
             canvas.toBlob(function (blob) {
-                const fileName = `${nameConvention}_${i}.webp`;
-                const url = URL.createObjectURL(blob);
-
-                const link = document.createElement("a");
-                link.download = fileName;
-                link.href = url;
-                link.setAttribute("directory", "converted-images");
-                link.click();
-
-                URL.revokeObjectURL(url);
+                const reader = new FileReader();
+                reader.onload = function () {
+                    const buffer = Buffer.from(reader.result);
+                    const fileName = namingConvention.replace('{name}', file.name).replace('{width}', width).replace('{height}', height);
+                    fs.writeFileSync(path.join(outputDir, fileName), buffer);
+                };
+                reader.readAsArrayBuffer(blob);
             });
         };
-
         img.src = URL.createObjectURL(file);
     }
 }
