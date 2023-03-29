032bb7a37cc806432ae55b03a9eefb4a3a6cb4af
Druhin Tarafder <druhinpro@gmail.com>
2023-03-29 19:52:40 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,66 +1,58 @@
-const convertBtn = document.getElementById("convertBtn");
-convertBtn.addEventListener("click", () => {
+function convertImages() {
     const clientName = document.getElementById("clientName").value;
     const serviceName = document.getElementById("serviceName").value;
-    const targetSize = document.getElementById("targetSize").value * 1024; // convert to bytes
-    const files = document.getElementById("imageFiles").files;
-
+    const targetSize = document.getElementById("targetSize").value * 1024; // Convert to bytes
+  
+    const fileInput = document.getElementById("fileInput");
+    const files = fileInput.files;
+  
     if (files.length === 0) {
-        alert("Please select at least one file to convert.");
-        return;
+      alert("Please select at least one file to convert.");
+      return;
     }
-
-    const namingConvention = `${clientName}_${serviceName}/`;
+  
     let counter = 1;
-    for (const file of files) {
-        const reader = new FileReader();
-        reader.readAsDataURL(file);
-        reader.onload = () => {
-            const img = new Image();
-            img.src = reader.result;
-            img.onload = () => {
-                const canvas = document.createElement("canvas");
-                const ctx = canvas.getContext("2d");
-                canvas.width = img.width;
-                canvas.height = img.height;
-                ctx.drawImage(img, 0, 0);
-                canvas.toBlob((blob) => {
-                    const newFileSize = blob.size;
-                    let quality = 1;
-                    while (newFileSize > targetSize) {
-                        quality -= 0.05;
-                        const webp = canvas.toDataURL("image/webp", quality);
-                        blob = dataURLtoBlob(webp);
-                        newFileSize = blob.size;
-                    }
-                    const url = URL.createObjectURL(blob);
-                    const a = document.createElement("a");
-                    a.href = url;
-                    a.download = `${namingConvention}image-${counter}.webp`;
-                    document.body.appendChild(a); // add link to page
-                    a.click(); // simulate click to trigger download
-                    document.body.removeChild(a); // remove link from page
-                    URL.revokeObjectURL(url);
-                    counter++;
-                    if (counter > files.length) {
-                        alert("All files converted and downloaded successfully.");
-                    }
-                }, "image/webp");
-            }
-        }
+  
+    for (let i = 0; i < files.length; i++) {
+      const file = files[i];
+      const image = new Image();
+      const reader = new FileReader();
+  
+      reader.onload = function(event) {
+        image.src = event.target.result;
+        image.onload = function() {
+          const canvas = document.createElement("canvas");
+          const ctx = canvas.getContext("2d");
+  
+          // Set canvas dimensions to match the image
+          canvas.width = image.width;
+          canvas.height = image.height;
+  
+          // Draw the image onto the canvas
+          ctx.drawImage(image, 0, 0, canvas.width, canvas.height);
+  
+          // Convert the canvas to a WebP image
+          canvas.toBlob(function(blob) {
+            const url = URL.createObjectURL(blob);
+            const link = document.createElement("a");
+            const fileName = `${clientName}/${serviceName}-image${counter}.webp`;
+  
+            // Download the WebP image
+            link.setAttribute("href", url);
+            link.setAttribute("download", fileName);
+            document.body.appendChild(link);
+            link.click();
+  
+            // Clean up
+            document.body.removeChild(link);
+            URL.revokeObjectURL(url);
+          }, "image/webp", 0.8);
+  
+          counter++;
+        };
+      };
+  
+      reader.readAsDataURL(file);
     }
-});
-
-function dataURLtoBlob(dataURL) {
-    const arr = dataURL.split(",");
-    const mime = arr[0].match(/:(.*?);/)[1];
-    const bstr = atob(arr[1]);
-    let n = bstr.length;
-    const u8arr = new Uint8Array(n);
-    while (n--) {
-        u8arr[n] = bstr.charCodeAt(n);
-    }
-    return new Blob([u8arr], {
-        type: mime
-    });
-}
+  }
+  
